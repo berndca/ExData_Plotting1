76646a6b80706ce6442a3ca5d0e1ef6f0ec6df9e
Bernd Meyer <berndca@gmail.com>
2020-04-15 18:55:20 +0100

    Removed legend box lines in plot4

--- plot4.R
+++ plot4.R
@@ -9,13 +9,13 @@ png(filename = "plot4.png")
 par(mfrow = c(2, 2))
 with(df2, {
     plot(Time, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
-    plot(Time, Voltage, type = "l", xlab = "", ylab = "Voltage")
+    plot(Time, Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
     plot(Time, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
     lines(Time, Sub_metering_2 , type = "l", col = "red")
     lines(Time, Sub_metering_3 , type = "l", col = "blue")
     legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-           col=c("black", "red", "blue"), lty = 1)
-    plot(Time, Global_reactive_power, type = "l", xlab = "")
+           col=c("black", "red", "blue"), lty = 1, box.lty=0)
+    plot(Time, Global_reactive_power, type = "l", xlab = "datetime")
 }
 )
 dev.off()
